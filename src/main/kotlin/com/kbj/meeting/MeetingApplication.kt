1a1d2cb9b26adfa3acf1abb0f042f3fe73dcf079
kbj0109 <kbj0109@gmail.com>
2024-05-11 21:53:31 +0900

    프로젝트 init

---
+++ src/main/kotlin/com/kbj/meeting/MeetingApplication.kt
@@ -0,0 +1,12 @@
+package com.kbj.meeting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeetingApplication
+
+fun main(args: Array<String>) {
+	runApplication<MeetingApplication>(*args)
+	System.out.println( 111)
+}
---
+++ src/test/kotlin/com/kbj/meeting/MeetingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kbj.meeting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MeetingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
