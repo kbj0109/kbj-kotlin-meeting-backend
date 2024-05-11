258532282ef7ae1ee82b047e5080a712d12819fd
kbj0109 <kbj0109@gmail.com>
2024-05-11 21:53:31 +0900

    코드 수정시 자동 서버 재시작

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,15 +21,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-mustache")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.withType<KotlinCompile> {
